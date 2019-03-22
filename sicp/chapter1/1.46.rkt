50ec2c6841c8ff8559915c20c2563fa81776cf6a
Dmitrii Esin <dmitrii_esin@epam.com>
2019-03-22 17:00:59 +0300

    test added

--- sicp/chapter1/1.46.rkt
+++ sicp/chapter1/1.46.rkt
@@ -2,24 +2,14 @@
 
 (require rackunit)
 
-(define (sqrt n) (* n n n)
-
-; 1
 (define (iterative-improove good-enough? improove)
-    (lambda (x) 
-        (if (good-enough? x)
-         x
-         ((iterative-improove good-enough? improove) (improove x)))))
-
-; 2
- (define (iterative-improove good-enough? improove)
-    (lambda (x)
-      (define (iter counter)
-        (if (good-enough? counter)
+   (lambda (x)
+     (define (iter counter)
+       (if (good-enough? counter)
         counter
         (iter (improove counter))))
-  (iter x))))
+     (iter x)))
 
-(define solution ((iterative-improove (lambda (x) (= x 64)) (lambda (x) (* x x))) 4))
+(define solution ((iterative-improove (lambda (x) (> x 1000)) (lambda (x) (* x x))) 100))
 
-(check-equal? solution 64)
+(check-equal? solution 10000)
